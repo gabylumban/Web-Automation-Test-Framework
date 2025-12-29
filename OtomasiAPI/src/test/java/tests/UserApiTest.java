2ed173688be5810fb72fa4f9116a5ed2927304b6
gabylumban <gabylumban@gmail.com>
2025-12-29 16:46:49 +0700

    Update UserApiTest.java

--- OtomasiAPI/src/test/java/tests/UserApiTest.java
+++ OtomasiAPI/src/test/java/tests/UserApiTest.java
@@ -1,37 +1,47 @@
+package tests;
+
+import config.BaseTest;
+import requests.UserRequest;
+import utils.TestDataUtil;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
+
 public class UserApiTest extends BaseTest {
 
     @Test
     void getUsers_validPage() {
         UserRequest.getUsers(TestDataUtil.VALID_PAGE)
-            .then()
-            .statusCode(200)
-            .body("data", not(empty()))
-            .log().all();
+                .then()
+                .statusCode(200)
+                .body("data", not(empty()))
+                .log().all();
     }
 
     @Test
     void getUserById_invalidId() {
         UserRequest.getUserById(TestDataUtil.INVALID_USER_ID)
-            .then()
-            .log().all()
-            .statusCode(404)
-            .body(equalTo("{}"));
+                .then()
+                .log().all()
+                .statusCode(404)
+                .body(equalTo("{}"));
     }
 
     @Test
     void getUsers_invalidPage() {
         UserRequest.getUsers(TestDataUtil.INVALID_PAGE)
-            .then()
-            .log().all()
-            .statusCode(200);
+                .then()
+                .log().all()
+                .statusCode(200);
     }
 
     @Test
     void getUsers_boundaryPageZero() {
         UserRequest.getUsers(TestDataUtil.BOUNDARY_PAGE)
-            .then()
-            .log().all()
-            .statusCode(200)
-            .body("page", equalTo(1));
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("page", equalTo(1));
     }
 }

3c03be47a37ced77c91a85e81c08f292275bbfa6
gabylumban <gabylumban@gmail.com>
2025-12-29 15:52:58 +0700

    Update RunCucumberTest.java

--- Web-test/src/test/java/RunCucumberTest.java
+++ Web-test/src/test/java/RunCucumberTest.java
@@ -1,11 +1,20 @@
 import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
+@IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdev")
+@ConfigurationParameter(
+        key = PLUGIN_PROPERTY_NAME,
+        value = "pretty," +
+                "html:build/cucumber-report/cucumber.html," +
+                "json:build/cucumber-report/cucumber.json"
+)
 public class RunCucumberTest {
 }

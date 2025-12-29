70badb4925a39fa7cc672dbe817a499b67303c53
gabylumban <gabylumban@gmail.com>
2025-12-29 17:21:55 +0700

    Update BaseTest.java

--- OtomasiAPI/src/test/java/config/BaseTest.java
+++ OtomasiAPI/src/test/java/config/BaseTest.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.BeforeAll;
 public class BaseTest {
 
     @BeforeAll
-    static void setup() {
+    static void setup(){
         RestAssured.baseURI = "https://reqres.in/api";
         RestAssured.requestSpecification =
                 RestAssured.given()
                         .header("x-api-key", "reqres_cc81c75fbb694842aafef73f927846ed")
                         .contentType("application/json");
     }
+
 }

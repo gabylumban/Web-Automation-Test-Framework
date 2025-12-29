912fdbed6d5f0430c4475d12a3c0f5c8c3e2129d
gabylumban <gabylumban@gmail.com>
2025-12-29 20:38:35 +0700

    Update UserApiStepDef.java

--- OtomasiAPI/src/test/java/stedefs/UserApiStepDef.java
+++ OtomasiAPI/src/test/java/stedefs/UserApiStepDef.java
@@ -1,49 +1,30 @@
-package stepdef;
+package stedefs;
 
-import io.cucumber.java.en.*;
+import config.BaseTest;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import requests.UserRequest;
 
-import static org.hamcrest.Matchers.*;
+public class UserApiStepDef extends BaseTest {
 
-public class UserApiStepDef {
+    Response response;
+    UserRequest userRequest = new UserRequest();
 
-    private Response response;
-    private int page;
-    private int userId;
-
-    @Given("I set page to {int}")
-    public void i_set_page_to(Integer page) {
-        this.page = page;
+    @Given("user service is available")
+    public void user_service_is_available() {
+        // setup sudah di BaseTest
     }
 
-    @Given("I set user id to {int}")
-    public void i_set_user_id_to(Integer id) {
-        this.userId = id;
+    @When("user sends request get user")
+    public void user_sends_request_get_user() {
+        response = userRequest.getUser();
     }
 
-    @When("I request GET /users")
-    public void i_request_get_users() {
-        response = UserRequest.getUsers(page);
-    }
-
-    @When("I request GET /users/{id}")
-    public void i_request_get_user_by_id() {
-        response = UserRequest.getUserById(userId);
-    }
-
-    @Then("I receive status code {int}")
-    public void i_receive_status_code(Integer statusCode) {
-        response.then().statusCode(statusCode);
-    }
-
-    @Then("response contains users list")
-    public void response_contains_users_list() {
-        response.then().body("data", not(empty()));
-    }
-
-    @Then("response page is {int}")
-    public void response_page_is(Integer expectedPage) {
-        response.then().body("page", equalTo(expectedPage));
+    @Then("response status code should be {int}")
+    public void response_status_code_should_be(Integer statusCode) {
+        Assert.assertEquals(statusCode.intValue(), response.getStatusCode());
     }
 }

5895a052b9b9def1fe6132a3d6493136b7c8cca2
gabylumban <gabylumban@gmail.com>
2025-12-29 15:54:19 +0700

    Update LoginPage.java

--- Web-test/src/test/java/page/LoginPage.java
+++ Web-test/src/test/java/page/LoginPage.java
@@ -2,18 +2,27 @@ package page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginPage {
+
     WebDriver driver;
+    WebDriverWait wait;
 
     By usernameInputText = By.id("user-name");
     By passwordInputText = By.id("password");
     By loginButton = By.id("login-button");
+    By errorMessage = By.cssSelector("[data-test='error']");
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     public void goToLoginPage() {
@@ -21,7 +30,8 @@ public class LoginPage {
     }
 
     public void inputUsername(String username) {
-        driver.findElement(usernameInputText).sendKeys(username);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(usernameInputText))
+                .sendKeys(username);
     }
 
     public void inputPassword(String password) {
@@ -32,7 +42,10 @@ public class LoginPage {
         driver.findElement(loginButton).click();
     }
 
-    public void validateErrorAppear(String errorMessage) {
-        assertTrue(driver.getPageSource().contains(errorMessage));
+    public void validateErrorAppear(String expectedMessage) {
+        WebElement error = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(errorMessage)
+        );
+        assertTrue(error.getText().contains(expectedMessage));
     }
 }
